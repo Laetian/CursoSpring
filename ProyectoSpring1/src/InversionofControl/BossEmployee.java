aeab61bb4e4452e71331b374eafcd79e688861ae
Laetian <laetianv@gmail.com>
2022-07-03 17:51:57 +0200

    FirstEx

---
+++ ProyectoSpring1/src/InversionofControl/BossEmployee.java
@@ -0,0 +1,10 @@
+package InversionofControl;
+
+public class BossEmployee {
+	
+	public String getTasks() {
+		
+		return "I manage issues related to my employees";
+	}
+
+}
---
+++ ProyectoSpring1/src/InversionofControl/UseEmployess.java
@@ -0,0 +1,17 @@
+package InversionofControl;
+
+public class UseEmployess {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Creación de objetos de tipo empleado
+		
+		BossEmployee Employee1= new BossEmployee();
+		
+		//Uso de los objetos creados
+		
+		System.out.println(Employee1.getTasks());
+	}
+
+}
